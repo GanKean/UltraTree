83c627e7e493a3c382420873a153beafcb9d78ed
GanKean <nimagag@kean.edu>
2026-03-26 12:03:26 -0400

    Update 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Windows;
+using System.Windows;
 
 namespace UltraTree;
 
@@ -11,17 +10,3 @@ public partial class MainWindow : Window
         DataContext = new MainViewModel();
     }
 }
-=======
-﻿using System.Windows;
-
-namespace UltraTree;
-
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
-        InitializeComponent();
-        DataContext = new MainViewModel();
-    }
-}
->>>>>>> b78f4c63145062f6840ee5adb0fbc7f8b5fe4d52
