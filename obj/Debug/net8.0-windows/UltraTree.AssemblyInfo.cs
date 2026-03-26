3ff6c15431a9213dccb8136ac60aaa6aca1cf294
GanKean <nimagag@kean.edu>
2026-03-26 19:58:49 -0400

    Update 29

--- obj/Debug/net8.0-windows/UltraTree.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/UltraTree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UltraTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e73e528c68027e079b9ad8f7ba6a4783b4bbcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908f281476fc8e05c8bbe50a567029ff92e98ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UltraTree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UltraTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
