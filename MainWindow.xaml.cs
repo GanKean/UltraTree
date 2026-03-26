a5b27f350bda510096de9721bec46d231eeb6c8b
GanKean <nimagag@kean.edu>
2026-03-26 14:45:38 -0400

    Update 11

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
+using System.Windows.Input;
 
 namespace UltraTree;
 
@@ -8,5 +11,37 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = new MainViewModel();
+    }
+
+    private void FoldersGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (FoldersGrid.SelectedItem is not ResultRow row)
+            return;
+
+        if (Directory.Exists(row.Path))
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                Arguments = $"\"{row.Path}\"",
+                UseShellExecute = true
+            });
+        }
+    }
+
+    private void FilesGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (FilesGrid.SelectedItem is not ResultRow row)
+            return;
+
+        if (File.Exists(row.Path))
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                Arguments = $"/select,\"{row.Path}\"",
+                UseShellExecute = true
+            });
+        }
     }
 }
